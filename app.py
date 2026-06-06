f23b3d7f969a1461615cc6766a74cd30336f3415
Ubuntu <ubuntu@ip-172-31-35-1.ec2.internal>
2026-06-06 08:09:33 +0000

    deployment test

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello from jenkins - Build 2")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "CI/CD Deployment Success - Build 3"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0' , port=5000)
